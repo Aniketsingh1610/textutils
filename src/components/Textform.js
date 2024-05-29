2b5e93c00493d1a42f1efa3a5d2ac6edbe23c5a1
Aniketsingh1610 <aniketsingh16oct2000@gmail.com>
2024-05-29 15:21:19 +0530

    Error fix

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 
-export default function Textform(props) 
-{
+export default function Textform(props) {
   const [text, settext] = useState("");
- 
+
   const handleupclick = () => {
     let newtext = text.toUpperCase();
     settext(newtext);
@@ -16,7 +15,7 @@ export default function Textform(props)
     props.showalert("Your Text is now Converted to LowerCase", "success")
 
   };
-  
+
   const handleClearAll = () => {
     let newtext = "";
     settext(newtext);
@@ -28,43 +27,40 @@ export default function Textform(props)
     console.log("ON change");
     settext(event.target.value);
   };
-  
+
   const [Mystyle, setmystyle] = useState({
     color: 'black',
     backgroundColor: 'white'
   });
-  const [btntext,setbtntext] = useState("Enable dark mode")
-  
- const Toggledarkmode = ()=>{
-    if(Mystyle.color == 'black'){
-      setmystyle({
-        color: 'white',
-        backgroundColor: 'black'
-      })
-      setbtntext("enable light mode")
-    }
-    else{
-      setmystyle({
-        color: 'black',
-      backgroundColor: 'white'
-    })
-    setbtntext("enable dark mode")
-  }
-  }
+
+  // const Toggledarkmode = () => {
+  //   if (Mystyle.color === 'black') {
+  //     setmystyle({
+  //       color: 'white',
+  //       backgroundColor: 'black'
+  //     })
+  //   }
+  //   else {
+  //     setmystyle({
+  //       color: 'black',
+  //       backgroundColor: 'white'
+  //     })
+  //   }
+  // }
 
 
 
   return (
-    <div style={{backgroundColor:props.mode==='dark'?'rgb(36 74 112)':'white'}} >
-      <div className="container" style={{backgroundColor:props.mode==='dark'?'rgb(36 74 112)':'white'}}>
+    <div style={{ backgroundColor: props.mode === 'dark' ? 'rgb(36 74 112)' : 'white' }} >
+      <div className="container" style={{ backgroundColor: props.mode === 'dark' ? 'rgb(36 74 112)' : 'white' }}>
         <h1 className="heading">Enter your text below:</h1>
-          <textarea className="form-control " style={{color:'black', backgroundColor:props.mode==='dark'?'rgb(63 101 141)':'white'}} onChange={handleonchange} id="floatingTextarea" value={text} rows='16'></textarea>
-       
+        <textarea className="form-control " style={{ color: 'black', backgroundColor: props.mode === 'dark' ? 'rgb(63 101 141)' : 'white' }} onChange={handleonchange} id="floatingTextarea" value={text} rows='16'></textarea>
+
         <button type="button" className="btn btn-primary mx-2 my-4" onClick={handleupclick}>Convert to Capital</button>
         <button type="button" className="btn btn-primary mx-2 my-4" onClick={handlelowclick}>Convert to Small</button>
         <button type="button" className="btn btn-primary mx-2 my-4" onClick={handleClearAll}>Clear All</button>
-        {/* <button className="button mx-2" style={Mystyle} onClick={Toggledarkmode}>{btntext}</button> */}
-    
+        {/* <button className="button mx-2" style={Mystyle} onClick={Toggledarkmode}></button> */}
+
       </div>
     </div>
   );
